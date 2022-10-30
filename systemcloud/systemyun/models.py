811e429602188f81b5c7d65613cb60cff1f367fd
huangshaozhe <97889667+huangshaozhe@users.noreply.github.com>
2022-10-30 18:03:04 +0800

    xg

--- systemcloud/systemyun/models.py
+++ systemcloud/systemyun/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Account(models.Model):
+class inger(models.Model):
     name = models.CharField(max_length=32)
-    password = models.CharField(max_length=32)
+    
