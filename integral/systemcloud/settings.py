f605ff1fb0b613d56e49354134296d2452057492
huangshaozhe <97889667+huangshaozhe@users.noreply.github.com>
2022-11-13 11:23:53 +0000

    xg

--- integral/systemcloud/settings.py
+++ integral/systemcloud/settings.py
@@ -123,7 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS=(
-#     os.path.join(BASE_DIR,'systemyun/static'),
-# )
-# 
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'systemyun/static'),
+)
